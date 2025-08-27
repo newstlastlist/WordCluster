da2c02b602a7678ada9a45737b81601ae44801bf
Viktor Rodrigo <viktor.rodrigo@tx-inc.com>
2025-08-27 15:41:54 +0300

    Game initializing script (GameAPP) + screen controller

---
+++ Assets/Scripts/App/GameApp.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure;
+using Shared;
+using UnityEngine;
+
+namespace App
+{
+    public sealed class GameApp : MonoBehaviour
+    {
+        [SerializeField] private ScreenController _screenController;
+
+        private ServiceRegistry _registry;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+
+            _registry = new ServiceRegistry();
+            Services.SetProvider(_registry);
+
+            var levelRepo = new JsonLevelRepository(
+                bundleResourcePath: "Levels/LevelsBundle",
+                folderPath: "Levels"
+            );
+
+            var progress = new PlayerPrefsProgressService();
+            progress.Load();
+
+            _registry.Register<ILevelRepository>(levelRepo);
+            _registry.Register<IProgressService>(progress);
+
+            _screenController.Show(ScreenId.Main);
+        }
+
+        private void OnApplicationQuit()
+        {
+            if (Services.TryGet<IProgressService>(out var progress))
+                progress.Save();
+        }
+    }
+}
---
+++ Assets/Scripts/App/ScreenController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace App
+{
+    public sealed class ScreenController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mainMenuPanel;
+        [SerializeField] private GameObject _gamePanel;
+        [SerializeField] private GameObject _winPanel;
+
+        public void Show(ScreenId id)
+        {
+            _mainMenuPanel.SetActive(id == ScreenId.Main);
+            _gamePanel.SetActive(id == ScreenId.Game);
+            _winPanel.SetActive(id == ScreenId.Win);
+        }
+    }
+
+    public enum ScreenId
+    {
+        Main,
+        Game,
+        Win
+    }
+
+}
