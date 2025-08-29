bc7fcebb7500a132f453f73297b333c9ef48ef6c
Viktor Rodrigo <viktor.rodrigo@tx-inc.com>
2025-08-29 13:35:06 +0300

    added D&D system (code)

---
+++ Assets/Scripts/UI/Game/CellDropHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.Game
+{
+    public sealed class CellDropHandler : MonoBehaviour, IDropHandler
+    {
+        private int _rowIndex;
+        private int _colIndex;
+
+        public int RowIndex => _rowIndex;
+        public int ColIndex => _colIndex;
+
+        public event Action<int, int, int> OnClusterDropped;
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            var clusterHandler = eventData.pointerDrag != null
+                ? eventData.pointerDrag.GetComponent<ClusterDragHandler>()
+                : null;
+
+            if (clusterHandler == null)
+            {
+                return;
+            }
+
+            int clusterId = clusterHandler.ClusterId;
+
+            if (OnClusterDropped != null)
+            {
+                OnClusterDropped.Invoke(clusterId, _rowIndex, _colIndex);
+            }
+        }
+
+        public void SetCoordinates(int rowIndex, int colIndex)
+        {
+            _rowIndex = rowIndex;
+            _colIndex = colIndex;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Game/ClusterDragHandler.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.Game
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public sealed class ClusterDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        private int _clusterId;
+        private CanvasGroup _canvasGroup;
+        private RectTransform _rectTransform;
+        private Transform _originalParent;
+        private Transform _rootCanvasTransform;
+
+        public int ClusterId => _clusterId;
+
+        public event Action<int, Vector2> OnDragged;
+        public event Action<int, Vector2> OnDragEnded;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _rectTransform = GetComponent<RectTransform>();
+            _originalParent = transform.parent;
+
+            var canvas = GetComponentInParent<Canvas>();
+            _rootCanvasTransform = canvas != null && canvas.rootCanvas != null
+                ? canvas.rootCanvas.transform
+                : _originalParent;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (_canvasGroup != null)
+            {
+                _canvasGroup.blocksRaycasts = false;
+                _canvasGroup.alpha = 0.6f;
+            }
+
+            if (_rootCanvasTransform != null)
+            {
+                transform.SetParent(_rootCanvasTransform);
+            }
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (_rectTransform != null)
+            {
+                _rectTransform.anchoredPosition += eventData.delta;
+            }
+
+            if (OnDragged != null)
+            {
+                OnDragged.Invoke(_clusterId, _rectTransform != null ? _rectTransform.anchoredPosition : Vector2.zero);
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (_canvasGroup != null)
+            {
+                _canvasGroup.blocksRaycasts = true;
+                _canvasGroup.alpha = 1f;
+            }
+
+            if (_originalParent != null)
+            {
+                transform.SetParent(_originalParent);
+            }
+
+            if (_rectTransform != null)
+            {
+                _rectTransform.anchoredPosition = Vector2.zero;
+            }
+
+            if (OnDragEnded != null)
+            {
+                OnDragEnded.Invoke(_clusterId, eventData.position);
+            }
+        }
+
+        public void SetClusterId(int clusterId)
+        {
+            _clusterId = clusterId;
+        }
+    }
+}
