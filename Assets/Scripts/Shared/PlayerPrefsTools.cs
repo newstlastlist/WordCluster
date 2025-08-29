c4fe39846733cd02ff720ad1e9c7d3bc64b22866
Viktor Rodrigo <viktor.rodrigo@tx-inc.com>
2025-08-29 19:16:05 +0300

    Player Prefs Utils

---
+++ Assets/Scripts/Shared/PlayerPrefsTools.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Shared
+{
+    public static class PlayerPrefsTools
+    {
+        [MenuItem("PlayerPrefs/Clear PlayerPrefs %#r")] // Ctrl+Shift+R (Win) / Cmd+Shift+R (Mac)
+        public static void ClearAllPlayerPrefs()
+        {
+            if (EditorUtility.DisplayDialog(
+                    "Clear PlayerPrefs",
+                    "Очистить все сохранения PlayerPrefs?",
+                    "Да", "Отмена"))
+            {
+                PlayerPrefs.DeleteAll();
+                PlayerPrefs.Save();
+                Debug.Log("[PlayerPrefsTools] Все PlayerPrefs очищены");
+            }
+        }
+    }
+}
+#endif
